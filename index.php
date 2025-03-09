d01e4deb3444ccfbe1cba41060768169db7c62a6
rackrunner <none@none>
2025-03-09 16:32:03 +0000

    - starting out

--- index.php
+++ index.php
@@ -1,4 +1,28 @@
 <html>
-    hello
-</html>
+    <head>
+        <link rel="stylesheet" href="general.css">
+        <!--Bootstrap CSS-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <!--jQuery JS-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    </head>
 
+    <body>
+        <div id="siteTop" class="navigation">
+            <div class="logo">
+                <img src="images/rack_runner_logo_2.png" alt="TEMPLATE">
+            </div>
+            <div class="links">
+                <div class="mainLink">
+                    <a href="index.htm">DASHBOARD</a>
+                </div>
+
+                <div class="mainLink">
+                    <a href="about_me.htm">ABOUT US</a>
+                </div>
+            </div>
+        </div>
+        <div id="main" style="margin-top: 60px;">
+            <div class="mainContent">                
+            </div>
+        </div>
