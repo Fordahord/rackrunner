8c37d1f7cbdaa7f2a407966bb8d9a94daf11cfec
Fordahord <grzech.czapla@gmail.com>
2025-04-02 13:26:25 +0000

    yii1

--- README.md
+++
@@ -1,2 +0,0 @@
-# rackrunner
-rackrunner
--- index.php
+++ index.php
@@ -1,30 +1,13 @@
-<html>
-    <head>
-        <link rel="stylesheet" href="general.css">
-        <!--Bootstrap CSS-->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <!--jQuery JS-->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    </head>
+<?php
 
-    <body>
-        <div id="siteTop" class="navigation">
-            <div class="logo">
-                <img src="images/rack_runner_logo_2.png" alt="TEMPLATE">
-            </div>
-            <div class="links">
-                <div class="mainLink">
-                    <a href="dashboard.html">DASHBOARD</a>
-                </div>
+// change the following paths if necessary
+$yii=dirname(__FILE__).'/../yii-1.1.27.8f9404/framework/yii.php';
+$config=dirname(__FILE__).'/protected/config/main.php';
 
-                <div class="mainLink">
-                    <a href="about_me.html">ABOUT US</a>
-                </div>
-            </div>
-        </div>
-        <div id="main" style="margin-top: 60px;">
-            <div class="mainContent">                
-            </div>
-        </div>
-        
-        Hello
+// remove the following lines when in production mode
+defined('YII_DEBUG') or define('YII_DEBUG',true);
+// specify how many levels of call stack should be shown in each log message
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+
+require_once($yii);
+Yii::createWebApplication($config)->run();
