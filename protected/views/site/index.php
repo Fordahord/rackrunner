c6158fbb268b8f76027b6829e25c1f7a78c49456
greg <none@none>
2025-04-24 16:43:34 +0100

    - remove navigation bar from index page

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -27,18 +27,6 @@ $this->pageTitle = Yii::app()->name;
 
 </head>
 <body id="page-top">
-    <!-- Navigation-->
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
-        <div class="container px-4 px-lg-5">
-            <img src="/images/rack_runner_logo.png" alt="Logo for the website" style="width:100px;">
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ms-auto my-2 my-lg-0">
-                    <li class="nav-item"><a class="nav-link" href="<?php echo Yii::app()->createUrl('/site/login'); ?>">Login</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
     <!-- Masthead-->
     <header class="masthead" alt= "Home page background">
         <div class="container px-4 px-lg-5 h-100">
