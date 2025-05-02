c363915103ce55a949ba5d210321ad8787ed982a
greg <none@none>
2025-05-02 12:07:55 +0100

    - make add product view look nicer

--- protected/views/product/create.php
+++ protected/views/product/create.php
@@ -1,44 +1,50 @@
-<div class="form">
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card p-4">
+                <h3 class="text-center mb-4">Add Product</h3>
 
-    <?php
-    $form = $this->beginWidget('CActiveForm', array(
-        'id'                     => 'product-form',
-        'enableClientValidation' => true,
-        'clientOptions'          => array(
-            'validateOnSubmit' => true,
-        ),
-        'htmlOptions'            => array('enctype' => 'multipart/form-data', 'aria-save-target' => true,),
-    ));
-    ?>
+                <div class="form">
+                    <?php
+                    $form = $this->beginWidget('CActiveForm', array(
+                        'id'                     => 'product-form',
+                        'enableClientValidation' => true,
+                        'clientOptions'          => array('validateOnSubmit' => true),
+                        'htmlOptions'            => array('enctype' => 'multipart/form-data'),
+                    ));
+                    ?>
 
-    <div class="row">
-        <?php echo $form->labelEx($model, 'name'); ?>
-        <?php echo $form->textField($model, 'name'); ?>
-        <?php echo $form->error($model, 'name'); ?>
+                    <div class="mb-3">
+                        <?php echo $form->labelEx($model, 'name'); ?>
+                        <?php echo $form->textField($model, 'name', array('class' => 'form-control')); ?>
+                        <?php echo $form->error($model, 'name'); ?>
+                    </div>
+
+                    <div class="mb-3">
+                        <?php echo $form->labelEx($model, 'description'); ?>
+                        <?php echo $form->textField($model, 'description', array('class' => 'form-control')); ?>
+                        <?php echo $form->error($model, 'description'); ?>
+                    </div>
+
+                    <div class="mb-3">
+                        <?php echo $form->labelEx($model, 'quantity'); ?>
+                        <?php echo $form->textField($model, 'quantity', array('class' => 'form-control')); ?>
+                        <?php echo $form->error($model, 'quantity'); ?>
+                    </div>
+
+                    <div class="mb-3">
+                        <?php echo $form->labelEx($model, 'image_path'); ?>
+                        <?php echo $form->fileField($model, 'image_path', array('class' => 'form-control')); ?>
+                        <?php echo $form->error($model, 'image_path'); ?>
+                    </div>
+
+                    <div class="text-center">
+                        <?php echo CHtml::submitButton('Submit', array('class' => 'btn btn-primary')); ?>
+                    </div>
+
+                    <?php $this->endWidget(); ?>
+                </div><!-- form -->
+            </div>
+        </div>
     </div>
-
-    <div class="row">
-        <?php echo $form->labelEx($model, 'description'); ?>
-        <?php echo $form->textField($model, 'description'); ?>
-        <?php echo $form->error($model, 'description'); ?>
-    </div>
-
-    <div class="row">
-        <?php echo $form->labelEx($model, 'quantity'); ?>
-        <?php echo $form->textField($model, 'quantity'); ?>
-        <?php echo $form->error($model, 'quantity'); ?>
-    </div>
-
-    <div class="row">
-        <?php echo $form->labelEx($model, 'image_path'); ?>
-        <?php echo $form->fileField($model, 'image_path'); ?>
-        <?php echo $form->error($model, 'image_path'); ?>
-    </div>
-
-    <div class="row buttons">
-        <?php echo CHtml::submitButton('Submit'); ?>
-    </div>
-
-    <?php $this->endWidget(); ?>
-
-</div><!-- form -->
+</div>
